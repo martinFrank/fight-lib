fb9301f3dea2ae1b7a5389f025deefaff6c6a7ae
Frank Martin <Martin.Frank@finnova.com>
2020-05-06 14:12:39 +0200

    initial comit

---
+++ src/test/java/com/github/martinfrank/fightlib/RoundTest.java
@@ -0,0 +1,15 @@
+package com.github.martinfrank.fightlib;
+
+import org.junit.Test;
+
+public class RoundTest {
+
+    @Test
+    public void testRound(){
+        //given
+
+        //when
+
+        //then
+    }
+}
